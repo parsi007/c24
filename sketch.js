84543c3f81f2498250df44e92d25bbd3f9f80085
parsi007 <76106066+parsi007@users.noreply.github.com>
2021-01-16 03:14:29 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+var pig1,log1;
+var bird1;
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810,350);
+    log1 = new Log(820,260,300,PI/2);
+    bird1 = new Bird(100,100);
+    ground = new Ground(600,height,1200,20)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    log1.display();
+    bird1.display();
+    pig1.display();
+}
